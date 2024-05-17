d9b5d34a777f378f45d1d1587f545a83c1f018af
Elham Fazel <55910811+fazelelham32@users.noreply.github.com>
2024-05-17 23:59:38 +0330

    Add files via upload
    Data mining with MATLAB-SOM

--- app1.m
+++ app1.m
@@ -2,59 +2,41 @@ clc;
 clear;
 close all;
 
-data=load('fisheriris');
+%% Load Data
 
-X=data.meas;
-Y=data.species;
+theta=linspace(-pi/2,+pi/2,100);
+r=unifrnd(1,1.5,size(theta));
 
-x{1}=X(1:50,:);
-x{2}=X(51:100,:);
-x{3}=X(101:150,:);
-x=x';
+X=[r.*cos(theta)
+   r.*sin(theta)];
 
-n=zeros(size(x));
-m=cell(size(x));
-S=cell(size(x));
-z=cell(size(x));
-is_normal=cell(size(x));
+%% SOM
 
-alpha=5;
+inputs = X;
 
-for i=1:numel(x)
-    
-    n(i)=size(x{i},1);
-    m{i}=mean(x{i});
-    S{i}=0;
-    for j=1:n(i)
-        S{i}=S{i}+(x{i}(j,:)-m{i})'*(x{i}(j,:)-m{i});
-    end
-    S{i}=S{i}/(n(i)-1);
-    
-    for j=1:n(i)
-        z{i}(j,:)=(x{i}(j,:)-m{i})*inv(S{i})*(x{i}(j,:)-m{i})';
-    end
-        
-    is_normal{i}=(z{i}<=alpha);
-    
-end
+% Create a Self-Organizing Map
+dimension1 = 1;
+dimension2 = 10;
+net = selforgmap([dimension1 dimension2]);
 
-%% Plot Results
+% Train the Network
+[net,tr] = train(net,inputs);
 
-dim1=3;
-dim2=4;
+% Test the Network
+outputs = net(inputs);
 
-figure;
-plot(x{1}(:,dim1),x{1}(:,dim2),'or','MarkerSize',10);
-hold on;
-plot(x{2}(:,dim1),x{2}(:,dim2),'ob','MarkerSize',10);
-plot(x{3}(:,dim1),x{3}(:,dim2),'ok','MarkerSize',10);
+IDX=vec2ind(outputs);
 
-plot(x{1}(~is_normal{1},dim1),x{1}(~is_normal{1},dim2),'xr','LineWidth',2,'MarkerSize',10);
-plot(x{2}(~is_normal{2},dim1),x{2}(~is_normal{2},dim2),'xb','LineWidth',2,'MarkerSize',10);
-plot(x{3}(~is_normal{3},dim1),x{3}(~is_normal{3},dim2),'xk','LineWidth',2,'MarkerSize',10);
+C=net.IW{1};
 
+% View the Network
+% view(net)
 
-
-
-
-
+% Plots
+% Uncomment these lines to enable various plots.
+%figure, plotsomtop(net)
+%figure, plotsomnc(net)
+%figure, plotsomnd(net)
+%figure, plotsomplanes(net)
+%figure, plotsomhits(net,inputs)
+%figure, plotsompos(net,inputs)
