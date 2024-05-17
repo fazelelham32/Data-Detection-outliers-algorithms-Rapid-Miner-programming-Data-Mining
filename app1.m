033d40d4a31c9f16b78d2c41a7f67f212701e687
Elham Fazel <55910811+fazelelham32@users.noreply.github.com>
2024-05-17 23:52:03 +0330

    Add files via upload
    Data mining with MATLAB-Outlier Detection

--- app1.m
+++ app1.m
@@ -2,20 +2,59 @@ clc;
 clear;
 close all;
 
-%% Load Data
+data=load('fisheriris');
 
-load fisheriris;
-X=meas;         % Inputs
-Y=species;      % Targets
+X=data.meas;
+Y=data.species;
 
-%% Naive Bayesian Classifier
+x{1}=X(1:50,:);
+x{2}=X(51:100,:);
+x{3}=X(101:150,:);
+x=x';
 
-bc=NaiveBayes.fit(X,Y);
+n=zeros(size(x));
+m=cell(size(x));
+S=cell(size(x));
+z=cell(size(x));
+is_normal=cell(size(x));
 
-Z=bc.predict(X);    % Outputs
+alpha=5;
 
-cm=confusionmat(Y,Z);
+for i=1:numel(x)
+    
+    n(i)=size(x{i},1);
+    m{i}=mean(x{i});
+    S{i}=0;
+    for j=1:n(i)
+        S{i}=S{i}+(x{i}(j,:)-m{i})'*(x{i}(j,:)-m{i});
+    end
+    S{i}=S{i}/(n(i)-1);
+    
+    for j=1:n(i)
+        z{i}(j,:)=(x{i}(j,:)-m{i})*inv(S{i})*(x{i}(j,:)-m{i})';
+    end
+        
+    is_normal{i}=(z{i}<=alpha);
+    
+end
 
-disp('Confusion Matrix:');
-disp(cm);
+%% Plot Results
+
+dim1=3;
+dim2=4;
+
+figure;
+plot(x{1}(:,dim1),x{1}(:,dim2),'or','MarkerSize',10);
+hold on;
+plot(x{2}(:,dim1),x{2}(:,dim2),'ob','MarkerSize',10);
+plot(x{3}(:,dim1),x{3}(:,dim2),'ok','MarkerSize',10);
+
+plot(x{1}(~is_normal{1},dim1),x{1}(~is_normal{1},dim2),'xr','LineWidth',2,'MarkerSize',10);
+plot(x{2}(~is_normal{2},dim1),x{2}(~is_normal{2},dim2),'xb','LineWidth',2,'MarkerSize',10);
+plot(x{3}(~is_normal{3},dim1),x{3}(~is_normal{3},dim2),'xk','LineWidth',2,'MarkerSize',10);
+
+
+
+
+
 
